179fb7a08a1b9ac175a662b9e109a5e77476e39b
Manuel Ricardo Zapata <manuelricardozapata@gmail.com>
2018-04-19 18:34:51 -0500

    Binary Gap problem.

---
+++ CodingChallenge.Library/BinaryGap.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CodingChallenge.Library
+{
+    public class BinaryGap
+    {
+
+        public int Solve(int N) {
+            string binaryNumber = Convert.ToString(N, 2);
+            int longestGap = 0;
+            int currentGap = 0;
+
+            for (int index = 0; index < binaryNumber.Length; index++)
+            {
+                if (binaryNumber[index] == '1')
+                {
+                    if (currentGap > longestGap)
+                    {
+                        longestGap = currentGap;
+                    }
+                    currentGap = 0;
+                }
+                else if (binaryNumber[index] == '0')
+                {
+                    currentGap++;
+                }
+            }
+
+            return longestGap;
+        }
+
+    }
+}
---
+++ CodingChallenge.Tests/BinaryGapTests.cs
@@ -0,0 +1,32 @@
+using CodingChallenge.Library;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodingChallenge.Tests
+{
+    [TestClass]
+    public class BinaryGapTests
+    {
+        [TestMethod]
+        public void SolutionTest()
+        {
+
+            var binaryGap = new BinaryGap();
+
+            int result = binaryGap.Solve(9);
+            Assert.AreEqual(2, result);
+
+            result = binaryGap.Solve(529);
+            Assert.AreEqual(4, result);
+
+            result = binaryGap.Solve(20);
+            Assert.AreEqual(1, result);
+
+            result = binaryGap.Solve(15);
+            Assert.AreEqual(0, result);
+
+            result = binaryGap.Solve(1041);
+            Assert.AreEqual(5, result);
+
+        }
+    }
+}
