09d59239bcab5fbe69068308056ac21710798b1a
Manuel Ricardo Zapata <manuelricardozapata@gmail.com>
2018-04-21 10:52:03 -0500

    Frog jump lesson.

---
+++ CodingChallenge.Library/FrogJump.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingChallenge.Library
+{
+    public class FrogJump
+    {
+
+        public int Solve(int X, int Y, int D) {
+            int distance = Y - X;
+            int jumpCount = distance / D;
+            if (distance % D > 0) {
+                jumpCount++;
+            }
+            return jumpCount;
+        }
+
+    }
+}
---
+++ CodingChallenge.Tests/FrogJumpTests.cs
@@ -0,0 +1,22 @@
+﻿using CodingChallenge.Library;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingChallenge.Tests
+{
+    [TestClass]
+    public class FrogJumpTests
+    {
+
+        [TestMethod]
+        public void FromJumpSolutionTest()
+        {
+            var frog = new FrogJump();
+            int result = frog.Solve(10, 85, 30);
+            Assert.AreEqual(3, result);
+        }
+
+    }
+}
