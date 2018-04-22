cffdf01f5ee4e84d252eb0f14c9e6d792de23017
manuelzapata <manuelricardozapata@gmail.com>
2018-04-21 22:09:59 -0500

    MaxCounters exercise.

---
+++ CodingChallenge.Library/Counting/MaxCounters.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingChallenge.Library.Counting
+{
+    public class MaxCounters
+    {
+
+        public int[] Solve(int N, int[] A) {
+
+            int[] counters = new int[N];
+            int maxCounter = 0;
+            int pendingMaxCounter = -1;
+
+            for (int index = 0; index < A.Length; index++)
+            {
+
+                if (A[index] == N + 1)
+                {
+                    pendingMaxCounter = maxCounter;
+                }
+                else {
+
+                    int counter = A[index] - 1;
+
+                    //There is a pending counter value to assign, so let's do it now.
+                    if (counters[counter] < pendingMaxCounter) {
+                        counters[counter] = pendingMaxCounter;
+                    }
+
+                    counters[counter] += 1;
+                    if (counters[counter] > maxCounter) {
+                        maxCounter = counters[counter];
+                    }
+                }
+            }
+
+            //There could be counters that never got the pending counter value assign.
+            for (int index = 0; index < counters.Length; index++)
+            {
+                if (counters[index] < pendingMaxCounter) {
+                    counters[index] = pendingMaxCounter;
+                }
+            }
+
+            return counters;
+        }
+
+        public int[] SolveFirstSolution(int N, int[] A)
+        {
+
+            int[] counters = new int[N];
+            int maxCounter = 0;
+
+            for (int index = 0; index < A.Length; index++)
+            {
+
+                if (A[index] == N + 1)
+                {
+                    //operation is max counter
+                    for (int counterIndex = 0; counterIndex < counters.Length; counterIndex++)
+                    {
+                        counters[counterIndex] = maxCounter;
+                    }
+                }
+                else
+                {
+
+                    int counter = A[index] - 1;
+                    counters[counter] += 1;
+                    if (counters[counter] > maxCounter)
+                    {
+                        maxCounter = counters[counter];
+                    }
+                }
+
+            }
+
+            return counters;
+        }
+
+    }
+}
---
+++ CodingChallenge.Tests/Counting/MaxCountersTests.cs
@@ -0,0 +1,33 @@
+﻿using CodingChallenge.Library.Counting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingChallenge.Tests.Counting
+{
+    [TestClass]
+    public class MaxCountersTests
+    {
+
+        [TestMethod]
+        public void MaxCountersSolutionTest()
+        {
+            var maxCounters = new MaxCounters();
+            int N = 5;
+            int[] array = { 3, 4, 4, 6, 1, 4, 4 };
+
+            int[] result = maxCounters.Solve(N, array);
+            int[] expected = new int[] { 3, 2, 2, 4, 2 };
+            CollectionAssert.AreEqual(expected, result);
+
+            N = 5;
+            array = new int[] { 6 };
+
+            result = maxCounters.Solve(N, array);
+            expected = new int[5];
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+    }
+}
