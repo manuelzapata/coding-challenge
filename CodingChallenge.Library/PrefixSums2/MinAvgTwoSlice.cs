fdc99db88e8b40ef57b1dba3ac4f8802639ce2bc
Manuel Ricardo Zapata <manuelricardozapata@gmail.com>
2018-04-23 17:13:07 -0500

    Slice with minimum average. 100% correct but perfomance is poor.

---
+++ CodingChallenge.Library/PrefixSums2/MinAvgTwoSlice.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingChallenge.Library.PrefixSums2
+{
+    public class MinAvgTwoSlice
+    {
+
+        public int Solve(int[] A) {
+
+            int[] prefixSumArray = new int[A.Length + 1];
+            //int[] suffixSumArray = new int[A.Length + 1];
+
+            for (int index = 0; index < A.Length; index++)
+            {
+                prefixSumArray[index + 1] = prefixSumArray[index] + A[index];
+                //suffixSumArray[index + 1] = suffixSumArray[index] + A[A.Length - index - 1];
+            }
+
+            double minAverage = double.MaxValue;
+            int minAverageStartingPosition = -1;
+
+            for (int index = 0; index < A.Length; index++)
+            {
+
+                for (int indexTwo = index + 1; indexTwo < A.Length; indexTwo++)
+                {
+                    int sum = prefixSumArray[indexTwo + 1] - prefixSumArray[index];
+                    double average = sum * 1.0 / (indexTwo - index + 1);
+
+                    if (average < minAverage)
+                    {
+                        minAverage = average;
+                        minAverageStartingPosition = index;
+                    }
+                    else if (average == minAverage)
+                    {
+                        minAverageStartingPosition = Math.Min(minAverageStartingPosition, index);
+                    }
+
+                }
+
+            }
+
+            //Find minimal average
+
+            return minAverageStartingPosition;
+        }
+
+    }
+}
---
+++ CodingChallenge.Tests/PrefixSums2/MinAvgTwoSliceTests.cs
@@ -0,0 +1,24 @@
+﻿using CodingChallenge.Library.PrefixSums2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingChallenge.Tests.PrefixSums2
+{
+    [TestClass]
+    public class MinAvgTwoSliceTests
+    {
+
+        [TestMethod]
+        public void MinAvgTwoSliceSolutionTest()
+        {
+            var sliceAverage = new MinAvgTwoSlice();
+            int[] array = { 4, 2, 2, 5, 1, 5, 8 };
+
+            int result = sliceAverage.Solve(array);
+            Assert.AreEqual(1, result);
+        }
+
+    }
+}
